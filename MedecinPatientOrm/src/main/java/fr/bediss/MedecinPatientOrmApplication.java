7aa3bcdc321cfb1e7e38ba77b6043e527e856a88
Bédiss NAHDI <nahdibediss@yahoo.fr>
2021-11-09 09:33:58 +0100

    Création Projet Spring boot : Medecin Patient

---
+++ MedecinPatientOrm/src/main/java/fr/bediss/MedecinPatientOrmApplication.java
@@ -0,0 +1,13 @@
+package fr.bediss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedecinPatientOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedecinPatientOrmApplication.class, args);
+	}
+
+}
